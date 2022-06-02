ec75a0ff96b6eead65bd5cab22294e4df7a56a51
Ilya_pinchuk <deffokorn71@gmail.com>
2022-06-02 21:19:41 +0300

    active link in sidebar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,20 +5,20 @@ import {NavLink} from "react-router-dom";
 const Navbar = () => {
     return (
         <nav className={classes.nav}>
-            <div className={`${classes.item} ${classes.active}`}>
-                <NavLink to="/profile" >Profile</NavLink>
+            <div className={classes.item} >
+                <NavLink  to="/profile" className = { navData => navData.isActive ? classes.active : classes.noActive } >Profile</NavLink>
             </div>
             <div className={classes.item}>
-                <NavLink to="/dialogs">Massages</NavLink>
+                <NavLink  to="/dialogs" className = { navData => navData.isActive ? classes.active : classes.noActive }>Massages</NavLink>
             </div>
             <div className={classes.item}>
-                <NavLink to="/news">News</NavLink>
+                <NavLink to="/news" className = { navData => navData.isActive ? classes.active : classes.noActive }>News</NavLink>
             </div>
             <div className={classes.item}>
-                <NavLink to="/music">Music</NavLink>
+                <NavLink to="/music" className = { navData => navData.isActive ? classes.active : classes.noActive }>Music</NavLink>
             </div>
             <div className={classes.item}>
-                <NavLink to="/settings">Settings</NavLink>
+                <NavLink to="/settings" className = { navData => navData.isActive ? classes.active : classes.noActive }>Settings</NavLink>
             </div>
         </nav>
     )
