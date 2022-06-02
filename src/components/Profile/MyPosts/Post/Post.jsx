5e147e9acc89801878fbc820ea0f02006bd36f25
Ilya_pinchuk <deffokorn71@gmail.com>
2022-06-02 16:25:07 +0300

    update 2.1

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -4,7 +4,7 @@ const Post = (props) => {
     return (
             <div className={classes.item}>
                 <img src ={props.img} alt=""/>
-                <span>{props.massage}</span>
+                <span className={classes.massage}>{props.massage}</span>
                 <div className={classes.wrapp_like}>
                     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Facebook_Thumb_icon.svg/640px-Facebook_Thumb_icon.svg.png" alt=""/>
                     <span>{props.count}</span>
