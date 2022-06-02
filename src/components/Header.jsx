992d4ee02da55635c559b33be3fe37cc203577bb
Ilya_pinchuk <deffokorn71@gmail.com>
2022-06-02 14:07:34 +0300

    update

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+  return (
+      <header className="header">
+          <img
+              src="https://pngimg.com/uploads/linkedIn/linkedIn_PNG12.png"
+              alt=""/>
+      </header>
+  )
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+const Profile = () => {
+  return (
+      <div className='content'>
+          <div className='img-wrapepr'>
+              <img
+                  src="https://cdn5.f-cdn.com/contestentries/1533674/20850863/5d2bac5b60bd2_thumb900.jpg"
+                  alt=""/>
+          </div>
+          <div>
+              ava
+          </div>
+          <div>
+              ny post
+              <div>
+                  new post
+              </div>
+              <div>post1</div>
+              <div>post 2</div>
+          </div>
+      </div>
+  )
+}
+export default Profile
